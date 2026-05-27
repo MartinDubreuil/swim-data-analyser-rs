396619485a53df8c0266c0543fc5c695284fb7d7
Martin Dubreuil <martin1dubreuil@gmail.com>
2026-05-27 15:04:08 +0200

    Use thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,62 +1,29 @@
-use std::fmt;
-
-use derive_more::From;
+use thiserror::Error;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug, From)]
+#[derive(Error, Debug)]
 pub enum Error {
     // Internal errors
-    MagicNumber {
-        received: String,
-    },
+    #[error("Mismatched Magic Number, received '{received}', expected '.FIT'")]
+    MagicNumber { received: String },
 
-    InvalidCrc {
-        received: u16,
-        expected: u16,
-    },
+    #[error("Mismatch CRC, received '{received:X}', expected '{expected:X}'")]
+    InvalidCrc { received: u16, expected: u16 },
 
-    InvaliHeaderSize {
-        received: u8,
-    },
+    #[error("Invalid header size, received '{received}', expected '14'")]
+    InvaliHeaderSize { received: u8 },
 
-    InvalidValue {
-        received: u8,
-        expected: u8,
-    },
+    #[error("Invalid value, received '{received}', expected '{expected}'")]
+    InvalidValue { received: u8, expected: u8 },
 
     // External errors
-    #[from]
-    Io(std::io::Error),
+    #[error("transparent")]
+    Io(#[from] std::io::Error),
 
-    #[from]
-    TryFromSliceError(std::array::TryFromSliceError),
+    #[error("transparent")]
+    TryFromSliceError(#[from] std::array::TryFromSliceError),
 
-    #[from]
-    Utf8Error(std::str::Utf8Error),
+    #[error("transparent")]
+    Utf8Error(#[from] std::str::Utf8Error),
 }
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Error::MagicNumber { received } => write!(
-                f,
-                "Mismatched Magic Number, received '{}', expected '.FIT'",
-                received
-            ),
-            Error::InvalidCrc { received, expected } => write!(
-                f,
-                "Mismatch CRC, received '{:X}', expected '{:X}'",
-                received, expected
-            ),
-            Error::InvaliHeaderSize { received } => write!(
-                f,
-                "Invalid header size, received '{}', expected '14'",
-                received
-            ),
-            _ => write!(f, "self:#?"),
-        }
-    }
-}
-
-impl std::error::Error for Error {}
